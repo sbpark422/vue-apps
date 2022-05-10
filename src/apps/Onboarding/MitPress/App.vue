1fbae752abcc203e2bef6b007e7eebc6ee7eb9af
Jisu Park <jspark474@gmail.com>
2022-05-10 01:27:59 -0400

    test xr-layer

--- src/apps/Onboarding/MitPress/App.vue
+++ src/apps/Onboarding/MitPress/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div> 
   <div id="room" class="darkwall">
-  <div class="spacer">
+  <div xr-layer class="spacer">
+  
   <Title msg="Reality Media" />
   <br />
   <div style="float:left; margin-right:20px">
-   <span xr-layer @click="linkClick" class="alink_img">
+   <span @click="linkClick" class="alink_img">
       <a href="https://mitpress.mit.edu/books/reality-media" target="_blank">
         <img class="img_Hover" src="../../../assets/images/Onboarding/realityMediaBook.jpg" height="383">
       </a>
     </span>
   </div>
 
-   <div class="squareoff"><div style="margin-left:30px">Published by <span xr-layer @click="linkClick" class="alink"><a href="https://mitpress.mit.edu/books/reality-media" target="_blank">MIT Press</a></span></div>
+   <div class="squareoff">
+    <div style="margin-left:30px">Published by <span @click="linkClick" class="alink"><a href="https://mitpress.mit.edu/books/reality-media" target="_blank">MIT Press</a></span>
+    </div>
   <br />
 
   <div class="oblique">By Jay David Bolter, Maria Engberg and Blair MacIntyre</div> 
@@ -20,6 +23,7 @@
   <br>
   How augmented reality and virtual reality are taking their places in contemporary media culture alongside film and television.
 </div>
+
   </div>
   </div>
   </div> 
