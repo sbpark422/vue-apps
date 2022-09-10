857cdaf1f00a51666a37609cf30ff53cc7910a8c
Jay Bolter <jdbolter@gatech.edu>
2022-09-10 16:29:23 -0400

    Changed Photo text

--- src/apps/History/Photography/App.vue
+++ src/apps/History/Photography/App.vue
@@ -10,7 +10,7 @@
   <br/>
   
 <div class="squareoff">
-Joseph Nicéphore Niépce created the earliest surviving photograph in the 1820s, using a camera obscura to project an image onto a pewter plate. Photography was quickly recognized as a medium with a special claim to capturing the real. As the British pioneer Fox Talbot put in his illustrated essay <span style="font-style:italics">The Pencil of Nature</span> (1844–1846), a photograph was an image “impressed by Nature’s hand.”
+Joseph Nicéphore Niépce created the earliest surviving photograph in the 1820s, using a camera obscura to project an image onto a pewter plate. Photography was quickly recognized as a medium with a special claim to capturing the real. As the British pioneer Fox Talbot put in his illustrated essay <span style="font-style:italic">The Pencil of Nature</span> (1844–1846), a photograph was an image “impressed by Nature’s hand.”
 </div>
 </div>
 	</div>
