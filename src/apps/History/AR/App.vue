132dae3355f3e63a53180d816bcfbf8211cdc156
Jay Bolter <jdbolter@gatech.edu>
2022-09-11 17:30:05 -0400

    Revise AR text

--- src/apps/History/AR/App.vue
+++ src/apps/History/AR/App.vue
@@ -5,12 +5,12 @@
   <div class="history_spacer-side">
   <Title class="history_title" msg="Augmented Reality" />
   
-  <p class="history_year">1968</p>
+  <p class="history_year">2010s</p>
   <hr class="history_hr">
   <br/>
   
 <div class="squareoff">
-Ivan Sutherland’s ‘The Sword of Damocles’ is considered the first head-mounted display for AR and VR. In the beginning, AR was most utilized for training purposes in the Air force. Now, its application could be seen everywhere, especially with the help of mobile devices and AR frameworks like ARkit and ARCore, developed by Apple and Google respectively. With this software development kit, AR became more widespread as AR could easily be incorporated into mobile applications.
+Sutherland’s ‘The Sword of Damocles’ depicted on level 2 is often considered the first head-mounted display for AR as well as VR. However, AR could not achieve its current status in our media culture until mobile phones with good graphics capabilities became widely avaible in the 2010s. Then, applications for games (e.g.Pokemon Go), navigation (e.g. Google Live View), social media (e.g. Instagram Stories AR Filter), and commerce (e.g. Ikea Place) all became possible.
 </div>
 </div>
 	</div>
